1f0c263ad823ecab625bc3dfe0b57f0c8524dca6
Gourav Rajak <gouravrajak+github@gmail.com>
2025-04-13 21:04:18 +0530

    fix: correct component import name from OrdersClient to OrderClient in orders page

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -3,7 +3,7 @@ import { format } from 'date-fns';
 import prismadb from '@/lib/prismadb';
 import { formatter } from '@/lib/utils';
 
-import { OrdersClient } from './components/client';
+import { OrderClient } from './components/client';
 import { OrderColumn } from './components/columns';
 
 const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
@@ -44,7 +44,7 @@ const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
-        <OrdersClient data={formattedOrders} />
+        <OrderClient data={formattedOrders} />
       </div>
     </div>
   );
